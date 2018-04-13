17556fcdc9ecb20627de564db8fedbde04145032
alex <alex.vlissidis@gmail.com>
2018-04-12 18:57:13 -0700

    bug fix

--- preprocessing/rouge.py
+++ preprocessing/rouge.py
@@ -9,16 +9,18 @@ import numpy as np
 
 class Rouge():
     def __init__(self, alpha=0.5):
+        self.alpha = alpha
+
+    def saliency(self, reference=None, system=None):
         self.r = Rouge155()
 
         self.r.model_dir = 'model_summaries'
         self.r.system_dir = 'system_summaries'
         self.r.system_filename_pattern = 'text.(\d+).txt'
         self.r.model_filename_pattern = 'text.[A-Z].#ID#.txt'
-     
-        self.alpha = alpha
 
-    def saliency(self, reference=None, system=None):
+        open('model_summaries/text.A.001.txt', 'w').close()
+        open('system_summaries/text.001.txt', 'w').close()
         if reference is not None:
             np.savetxt("model_summaries/text.A.001.txt", reference, newline="\n", fmt="%s")
         
@@ -33,8 +35,14 @@ class Rouge():
         return self.alpha * R1 + (1 - self.alpha) * R2
 
 if __name__ == "__main__":
-    ref = np.array(["blah blah blah", "motherfucker"])
-    model = np.array(["motherfucker", "blah"])
+    ref = np.array([" The territory's stock market opened this morning sharply lower"])
+    model = np.array(["      Deteriorating relations between Britain and China over Hong Kong's political future has cast a pall over the colony's financial markets",
+         ' Governor Patten has proposed elections in 1994 and 1995 to allow for greater democracy for the people and is supported by the people',
+          ' Patten has repeatedly indicated a go-it-alone possibility marking a change in British policy toward Hong Kong and China, which had placed priority on reaching agreement with Beijing',
+           '  China claims that financing proposals from the Hong Kong government would put a heavy financial burden on the post-1997 community',
+            '  China could sacrifice the economic well-being of its people in response to a perceived threat to its authority',
+             ' '])
 
     r = Rouge()
+    print(r.saliency(ref, model))
     print(r.saliency(ref, model))
