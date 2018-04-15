b76dbef46c1f6ad735861131ea8e983dad4ec00b
alex <alex.vlissidis@gmail.com>
2018-04-15 16:35:47 -0700

    training and plotting and saving model

--- train.py
+++ train.py
@@ -1,112 +1,107 @@
-# -*- coding: utf-8 -*-
 """
 Created on Sat Mar 17 11:01:27 2018
 
 @author: leo
 """
-
-import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+import pickle
 import numpy as np
-import gensim
+import matplotlib.pyplot as plt
 
-from sklearn.metrics import accuracy_score
-from keras.preprocessing import sequence
-from keras.utils import np_utils
-from keras.layers.merge import Concatenate
-from keras.models import Sequential, Model
-from keras.layers.core import Dense, Dropout, Activation, Lambda
-from keras.layers.embeddings import Embedding
-from keras.layers import SpatialDropout1D, Flatten, MaxPooling1D , Input
-from keras.preprocessing.text import Tokenizer
-from keras.layers.convolutional import Conv1D
-from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers import MaxPooling2D
+from keras.layers.convolutional import Conv2D
+from keras import regularizers
 
-# ---------------------- Parameters start -----------------------
-model_type = "CNN-rand"  # CNN-rand|CNN-word2vec
-summarization = "single" # single|multi
-
-# Model Hyperparameters
-word_limit = 50000
-embedding_dim = 300
-window_size = 3
-num_filters = 400
-dropout_prob = 0.5
-max_features = 10000
-
-# Training parameters
-batch_size = 64
-nb_epoch = 20
-
-# ---------------------- Parameters end -----------------------
-
-
-
-
-# ---------------------- Preprocessing start -----------------------
-
-
-
-
-
-# Prepare embedding layer weights and convert inputs for static model
-if model_type = "CNN-word2vec":
-    embedding_model = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True, limit=word_limit)
-    embedding_weights = {key: embedding_model[word] if word in embedding_model else
-                              np.random.uniform(-0.25, 0.25, embedding_model.vector_size)
-                         for key, word in vocabulary_inv.items()}
-     x_train = np.stack([np.stack([embedding_weights[word] for word in sentence]) for sentence in x_train])
-     x_test = np.stack([np.stack([embedding_weights[word] for word in sentence]) for sentence in x_test])
-     print("x_train static shape:", x_train.shape)
-     print("x_test static shape:", x_test.shape)
-    
-elif model_type == "CNN-rand":
-    embedding_weights = None
-
-else:
-    raise ValueError("Unknown model type")
-    
-# ---------------------- Preprocessing end -----------------------
-    
-
-# ---------------------- Model start -----------------------
+#from sklearn.model_selection import train_test_split
 
 # Build model
-print('Build model...')
-model = Sequential()
-model.add(Embedding(max_features,300))
-# we add a Convolution 1D, which will learn nb_filter
-# word group filters of size window_size:
-model.add(Conv1D(filters=num_filters,
-                         kernel_size=window_sizedow_size,
-                         padding="valid",
-                         activation="relu",
-                         strides=1))
-#Max pooling
-def max_1d(X):
-    return K.max(X, axis=1)
+def build_model(input_shape, conv_window_size, num_filters, reg, dropout):
+    model = Sequential()
+    #model.add(Embedding(max_features,300))
 
-model.add(Lambda(max_1d, output_shape=(nb_filter,)))
+    # we add a Convolution 1D, which will learn num_filters
+    # word group filters of size conv_window_size:
+    print(input_shape)
+    model.add(Conv2D(input_shape=input_shape,
+                        filters=num_filters,
+                        kernel_size=(1, conv_window_size),
+                        padding="valid",
+                        activation="relu",
+                        strides=1,
+                        data_format='channels_first'))
+    
+    model.add(MaxPooling2D(pool_size=(400, 1)))
 
-#Fully Connected + Dropout + softmax
-model.add(Dropout(dropout_prob)) 
-model.add(Dense(1, activation='sigmoid'))
+    #Fully Connected + Dropout + sigmoid
+    model.add(Dropout(dropout))
+    model.add(Flatten())
+    model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(reg)))
+    
+    #In addition, an l2−norm constraint of the weights w_r is imposed during training as well
 
-# ---------------------- Model end -----------------------
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adadelta',
+                  metrics=['mae'])
+    return model
 
-# ---------------------- Training/Testing start-----------------------
+def train(model, x_train, y_train, val_train_ratio=0.2, epochs=1000, batch_size=128):
+    history = model.fit(x_train, y_train,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        validation_split=val_train_ratio,
+                        shuffle=True,
+                        verbose=1)
+    return history
 
-#In addition, an l2−norm constraint of the weights w_r is imposed during training as well
+def load_data():
+    data = pickle.load(open("preprocessing/wordEmbeddingsToSaliency.pickle", "rb"))
+    x_raw = data[::2]
+    y = data[1::2]
 
-model.compile(loss='binary_crossentropy',
-              optimizer='adadelta',
-              metrics=['accuracy'])
+    x = list()
+    for emb in x_raw:
+        x.append(emb.tolist())
 
+    x = np.array(x)
+    x = np.expand_dims(x, axis=1)
+    return x, y
 
-print('Train...')
-model.fit(X_train, Y_train, batch_size=batch_size, epochs=nb_epoch,
-          validation_data=(X_test, Y_test))
-score, acc = model.evaluate(X_test, Y_test,
-                            batch_size=batch_size)
-print('Test score:', score)
-print('Test accuracy:', acc)
-# ---------------------- Training/Testing end-----------------------
+def main():
+    # Model Hyperparameters
+    conv_window_size = 300
+    num_filters = 400
+    reg = 0.01
+    dropout = 0.5
+    
+    # Training parameters
+    epochs = 10
+    batch_size = 128
+    test_train_ratio = 0.2
+    val_train_ratio = 0.2
+
+    x_train, y_train = load_data()
+
+    #x_train, x_test, y_train, y_test = test_train_split(x, y, test_size=test_train_ratio)
+
+    model = build_model((1, x_train.shape[2], x_train.shape[3]), conv_window_size, num_filters, reg, dropout)
+    history = train(model, x_train, y_train, val_train_ratio, epochs, batch_size)
+
+    model.model.save('model.h5') 
+
+    f, (ax1, ax2) = plt.subplots(2, 1)
+    ax1.plot(range(1, epochs+1), history.history['val_mean_absolute_error'], 'tab:blue', label="validation MAE")
+    ax1.plot(range(1, epochs+1), history.history['mean_absolute_error'], 'tab:red', label="training MAE")
+
+    ax2.plot(range(1, epochs+1), history.history['loss'], 'tab:orange', label="loss")
+    ax2.plot(range(1, epochs+1), history.history['val_loss'], 'tab:green', label="validation loss")
+
+    ax1.legend()
+    ax2.legend()
+
+    f.savefig('training.png', dpi=300)
+
+if __name__ == "__main__":
+    main()
