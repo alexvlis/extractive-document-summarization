f424d4b9426019cfd8dbf3a14e47cd5a39256b8b
LeoLaugier <leojlaugier@gmail.com>
2018-04-20 13:46:38 -0700

    Debugged test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import numpy as np
 from keras.models import load_model
-from preprocessing.dataload import loadDUC
-from preprocessing.rouge import Rouge
+from preprocessing.dataload import loadTestData
+#from preprocessing.rouge import Rouge
 
 def dummy_rouge(sentence, summary, alpha=0.5):
     if sentence in summary:
@@ -11,11 +11,11 @@ def dummy_rouge(sentence, summary, alpha=0.5):
 def dummy_loadTestData():
     testing_data = [ [ np.array(["This sentence is important for doc0." ,
                                  "Such a sentence is irrelevent for doc 0."]), 
-                       np.random.rand(2,1,5,300), 
+                       np.random.rand(2,5,300), 
                        np.array(["This sentence is important for doc0."]) ],
                      [ np.array(["Lol that sentence is awesome for do1." , 
                                  "No way, this is irrelevent"]), 
-                       np.random.rand(2,1,5,300), 
+                       np.random.rand(2,5,300), 
                                 np.array(["Lol that sentence is awesome for do1."]) ] ]
     return testing_data
 
@@ -27,8 +27,8 @@ def test(model, testing_data, batch_size = 128, upper_bound = 100, threshold = 0
         
         Parameters: 
             testing_data           - np.array 
-                                        ex: np.array([ doc1, doc2, ... , docn])
-                                         where doci = np.array([sentences, x_test, summary])
+                                        ex: [ doc1, doc2, ... , docn]
+                                         where doci = [sentences, x_test, summary]
                                              where sentences = np.array of string
                                                    x_test = np.array of matrices (embedded sentences)
                                                    summaries = np.array of sentences
@@ -42,10 +42,12 @@ def test(model, testing_data, batch_size = 128, upper_bound = 100, threshold = 0
         sentences = doc[0]
         
         x_test_old = doc[1]
-        (s1,s2,s3,s4) = x_test_old.shape
-        x_test = np.random.rand(s1,s2,190,s4)
+        s1 = x_test_old.shape[0]
+        (s3,s4) = x_test_old[0].shape
+        print(s1,s3,s4)
+        x_test = np.random.rand(s1,1,190,s4)
         for i in range(s1) :
-            x_test[i] = np.array( [ np.pad(x_test_old[i][0], ((190-5,0),(0,0)), 'constant') ] )
+            x_test[i] = np.array( [ np.pad(x_test_old[i], ((190-s3,0),(0,0)), 'constant') ] )
             
 
         true_summary = doc[2]
@@ -80,8 +82,9 @@ def test(model, testing_data, batch_size = 128, upper_bound = 100, threshold = 0
 
 def main():
     model = load_model('model.h5')
-    
-    testing_data = dummy_loadTestData()
+    #testing_data = dummy_loadTestData()
+    testing_data = loadTestData("./data/DUC2002_Summarization_Documents")
+    print(testing_data)
     
     rouge1_score = test(model, testing_data, upper_bound=5, metric = "ROUGE1")
     #rouge2_score = test(model, testing_data, upper_bound=5, metric = "ROUGE2")
